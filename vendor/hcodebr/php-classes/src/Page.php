228d20277560cc5b2ca7de85ce82aecd949aaa4f
Gabrielle <gabrielleolives1@gmail.com>
2019-05-24 12:18:23 -0300

    criando pagina do administrador e a rota para a mesma

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -13,7 +13,7 @@ class Page{
 
 
 //criando metodos magicos
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir ="/ecommerce/views/"){
 		
 		//quero que o que a pessoa informou como parametro no array opts da classe construtura sobrescreva no array defaults 
 		//se eu passar um parametro no opts e der conflito com o defaults, vale o opts
@@ -21,7 +21,7 @@ class Page{
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. "/ecommerce/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views-cache/",
 			"debug"         => false // set to false to improve the speed
 				   );
@@ -57,15 +57,13 @@ class Page{
 
 		//body html
 		public function setTpl($name, $data = array(), $returnHTML = false){
-
 			$this->setData($data);
-
 			//desenhando um template na tela 
 			return $this->tpl->draw($name, $data, $returnHTML);
 		}
 
 
-//criando o footer
+		//criando o footer
 		public function __destruct(){
 			$this->tpl->draw("footer");
 		}
